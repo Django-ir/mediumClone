adfa9aadcc53f4be436c855af9a29ba4f7364ae0
sinakoosha <sinkoosha@gmail.com>
2022-12-11 17:23:48 +0330

    feedBox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from "next/app";
-
+import { Provider } from "react-redux";
 import "../styles/globals.css";
+import { store } from "../redux/store";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
 }
