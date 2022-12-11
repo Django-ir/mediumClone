ca1c13c1e375bd04fb74466d4f185d12020336e1
alimh17 <amohamadi17@gamil.com>
2022-12-11 12:11:41 +0330

    login register

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
